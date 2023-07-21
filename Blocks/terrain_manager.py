576e759311b20707fdd9b0b374d5462f11010239
Vierce1 <a.westermann.19@gmail.com>
2023-07-21 09:11:39 -0500

    working on water

--- Blocks/terrain_manager.py
+++ Blocks/terrain_manager.py
@@ -245,7 +245,7 @@ class Terrain_Manager:
         self.matrix[block.position[0], block.position[1]] = block.id
         return
 
-    def flow(self, block: Block, flow: (int, int))-> None:
+    # def flow(self, block: Block, flow: (int, int))-> None:
 
 
 
