017647d74c206d88a2b024bef569c90d217e2751
Vierce1 <a.westermann.19@gmail.com>
2023-07-23 10:19:54 -0500

    ghosting trails

--- Blocks/block.py
+++ Blocks/block.py
@@ -7,8 +7,8 @@ import Blocks.block_type
 # @jitclass
 class Block:
     # slots reduces size of object by only reserving enough memory to hold these values.
-    # __slots__ = ('id', 'type', 'position', 'vert_velocity', 'horiz_velocity',
-    #              'collision_detection')
+    __slots__ = ('id', 'type', 'position', 'vert_velocity', 'horiz_velocity',
+                 'collision_detection')
     def __init__(self, type: block_type.Block_Type, position: (int, int)):
         # print(sys.getsizeof(self ))
         self.id = 0
@@ -17,3 +17,12 @@ class Block:
         self.vert_velocity = 0
         self.horiz_velocity = 0
         self.collision_detection = not type.rigid  # False for rigid=True blocks
+
+
+class Ghost_Particle:
+    __slots__ = ('position', 'parent_id', 'color')
+    def __init__(self, position: (int, int), parent_id: int, color: (int, int, int)):
+        self.position = position
+        self.parent_id = parent_id
+        self.color = color
+        # Do I need an id?
